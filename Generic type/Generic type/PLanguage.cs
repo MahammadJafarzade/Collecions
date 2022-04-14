912277b787657493962270ee25f385bea9bfa25c
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-04-14 12:36:20 +0400

    collections

---
+++ Generic type/Generic type/PLanguage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_type
+{
+    class PLanguage
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public PLanguage(string name,int year)
+        {
+            Name = name;
+            Year = year;
+
+        }
+
+    }
+}
---
+++ Generic type/Generic type/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PLanguage pl1 = new PLanguage("Python", 1991);
+            PLanguage pl2 = new PLanguage("C++",1983 );
+            PLanguage pl3 = new PLanguage("C#", 2001);
+            PLanguage pl4 = new PLanguage("Java",1995);
+            List<PLanguage> pl = new List<PLanguage>();
+            pl.Add(pl1);
+            pl.Add(pl2);
+            pl.Add(pl3);
+            pl.Add(pl4);
+            pl.Sort((x, y) => x.Year - y.Year);
+            
+
+            foreach (var item in pl)
+            {
+                Console.WriteLine(item.Year);
+            }
+            //foreach (var item in pl)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+            foreach (var item in pl)
+            {
+                Console.WriteLine($"{item.Name} and {item.Year}");
+            }
+           
+
+
+
+
+
+
+        }
+    }
+}
+    
+
